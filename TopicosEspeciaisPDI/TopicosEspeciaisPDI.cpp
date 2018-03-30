9de8970fbc5b8649ceb08bd38d4ffd1da6480bce
Adson Marques da Silva Esteves <3798925@univali.br>
2018-03-29 22:19:25 -0300

    suavizacao e alargamaento

--- TopicosEspeciaisPDI/TopicosEspeciaisPDI.cpp
+++ TopicosEspeciaisPDI/TopicosEspeciaisPDI.cpp
@@ -10,26 +10,38 @@ Mat negativo(Mat imagemBase);
 Mat escalaCinza(Mat imagemColorida);
 Mat limiarizacao(Mat imagemBase, int limiar);
 Mat logaritmo(Mat imagemBase);
+Mat potencia(Mat imagemBase, double gamma);
+Mat janelamento(Mat imagemBase, int limiar_inf, int limiar_sup);
+Mat suavizacao(Mat imagemBase);
 
 Mat escalaCinzaOPenCV(Mat imagemColorida);
+
+int mat[5][5]={ 1,1,1,1,1 ,
+				1,1,1,1,1 ,
+				1,1,1,1,1 ,
+				1,1,1,1,1 ,
+				1,1,1,1,1 };
 
 int main()
 {
 	Mat imagemTeste;
 	Mat imagemNegativo;
 	Mat imagemGrayscale;
-	imagemTeste = imread("Imagens/log.tif");
+	imagemTeste = imread("Imagens/placa.tif");
 	imagemNegativo = negativo(imagemTeste);
 	imagemGrayscale = escalaCinza(imagemTeste);
 	imshow("Teste", imagemTeste);
-	imshow("Negativo", imagemNegativo);
+	/*imshow("Negativo", imagemNegativo);
 	imshow("Cinza", imagemGrayscale);
 	imshow("OPCV", escalaCinzaOPenCV(imagemTeste));
-	/*imshow("BLUE", canal(imagemTeste,0));
+	imshow("BLUE", canal(imagemTeste,0));
 	imshow("VERDE", canal(imagemTeste,1));
-	imshow("VERMELHO", canal(imagemTeste,2));*/
-	imshow("LIMIAR", escalaCinzaOPenCV(limiarizacao(imagemTeste, 125)));
+	imshow("VERMELHO", canal(imagemTeste,2));
+	imshow("LIMIAR", limiarizacao(escalaCinza(imagemTeste), 125));
 	imshow("LOGARITMO", logaritmo(imagemTeste));
+	imshow("POTENCIA", potencia(imagemTeste, 2));*/
+	//imshow("Janelamento", janelamento(imagemTeste, 90, 138));
+	imshow("Suavizaçãop", suavizacao(imagemTeste));
 	waitKey(0);
     
 	return 0;
@@ -123,6 +135,59 @@ Mat limiarizacao(Mat imagemBase, int limiar) {
 	return aux;
 }
 
+Mat janelamento(Mat imagemBase, int li, int ls) {
+	Mat aux = imagemBase.clone();
+	aux = escalaCinzaOPenCV(aux);
+	for (int x = 0; x < aux.rows; x++) {
+		for (int y = 0; y < aux.cols; y++) {
+			uchar pixel = aux.at<uchar>(x, y);
+
+
+			if (pixel >= ls)
+			{
+				pixel = 255;
+			}
+			else if (pixel <= li)
+			{
+				pixel = 0;
+			}
+			else
+			{
+				pixel = (pixel - li) * (255 / (ls - li));
+			}
+
+			aux.at<uchar>(x, y) = pixel;
+		}
+	}
+
+	return aux;
+}
+Mat suavizacao(Mat imagemBase)
+{
+	Mat aux = imagemBase.clone();
+	aux = escalaCinzaOPenCV(aux);
+	for (int x = 2; x < aux.rows-2; x++) {
+		for (int y = 2; y < aux.cols-2; y++) {
+			uchar pixel = aux.at<uchar>(x, y);
+
+			double soma = 0;
+
+			for (int i = 0, xi = x-2; i < 5; i++, xi++)
+			{
+				for (int j = 0, yj = y-2; j < 5; j++, yj++)
+				{
+					soma += mat[i][j] * aux.at<uchar>(xi, yj);
+				}
+			}
+
+			pixel = soma / 25;
+
+			aux.at<uchar>(x, y) = pixel;
+		}
+	}
+
+	return aux;
+}
 Mat logaritmo(Mat imagemBase) {
 	Mat aux = escalaCinzaOPenCV(imagemBase.clone());
 	for (int x = 0; x < aux.rows; x++) {
@@ -132,6 +197,23 @@ Mat logaritmo(Mat imagemBase) {
 			double c = 255 / log(256);
 
 			pixel = c * log(1 + pixel);
+
+			aux.at<uchar>(x, y) = pixel;
+		}
+	}
+
+	return aux;
+}
+
+Mat potencia(Mat imagemBase, double gamma) {
+	Mat aux = escalaCinzaOPenCV(imagemBase.clone());
+	for (int x = 0; x < aux.rows; x++) {
+		for (int y = 0; y < aux.cols; y++) {
+			uchar pixel = aux.at<uchar>(x, y);
+
+			double c = 255 / pow(256, gamma);
+
+			pixel = c * pow(pixel,gamma);
 
 			aux.at<uchar>(x, y) = pixel;
 		}
